cfcb7d0e55a37c3b11b2e1e0b2ec4dd3229e12fd
kcf-jackson <kcf.jackson@gmail.com>
2019-10-05 01:10:33 +1000

    Refactor (string-)rewrite function

--- R/string.R
+++ R/string.R
@@ -1,37 +1,37 @@
-rewrite_new <- function(x) {
-    pat <- "[a-zA-Z_]+[$]new"
+split_str <- function(x, split) {
+    unlist(strsplit(x, split))
+}
+
+rewrite_KW <- function(x, pat, rewrite) {
     from <- unlist(stringr::str_extract_all(x, pattern = pat))
     if (purrr::is_empty(from)) return(x)
-
-    rewrite <- function(x) {
-        s <- unlist(strsplit(x, split = "[$]"))
-        paste(rev(s), collapse = " ")
-    }
     to <- purrr::map(from, rewrite)
 
     fun_list <- purrr::map2(from, to, function(from, to) {
         f(x, gsub(x = x, pattern = from, replacement = to, fixed = T))
     })
-
     freduce(x, fun_list)
 }
 
 
+rewrite_new <- function(x) {
+    pattern <- "[a-zA-Z_]+[$]new"
+    rewrite <- function(x) {
+        x %>%
+            split_str(split = "[$]") %>%
+            rev() %>% paste(collapse = " ")
+    }
+    rewrite_KW(x, pattern, rewrite)
+}
+
 rewrite_dot <- function(x) {
-    pat <- "[a-zA-Z0-9_]+([$][a-zA-Z0-9_]+)+"
-    from <- unlist(stringr::str_extract_all(x, pattern = pat))
+    pattern <- "[a-zA-Z0-9_]+([$][a-zA-Z0-9_]+)+"
+    rewrite <- f(x, gsub("[$]", ".", x))
+    rewrite_KW(x, pattern, rewrite)
+}
 
-    # Need to order `from' by length of string, because gsub may replace
-    # a substring contained in a longer string and this changes the longer
-    # string making the subsequent gsub ineffective
-    from <- from[order(nchar(from), decreasing = T)]
-    if (purrr::is_empty(from)) return(x)
-
-    to <- purrr::map(from, f(x, gsub("[$]", ".", x)))
-
-    fun_list <- purrr::map2(from, to, function(from, to) {
-        f(x, gsub(x = x, pattern = from, replacement = to, fixed = T))
-    })
-
-    freduce(x, fun_list)
+rewrite_for_loop <- function(x) {
+    pattern <- "[(][a-z]+ in [^{]+"
+    rewrite <- f(x, gsub("[$]", ".", x))
+    rewrite_KW(x, pattern, rewrite)
 }

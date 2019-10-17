117a4e2d7aa748ce727e34f0847d7292627ab5b1
kcf-jackson <kcf.jackson@gmail.com>
2019-10-17 22:18:55 +1100

    Add conditional rewriting

--- R/compile_file.R
+++ R/compile_file.R
@@ -17,7 +17,8 @@ compile_r <- function(input, output = "") {
 rewrite <- function(ast) {
   ast %>%
     rewrite_new() %>%
-    rewrite_by_subst()
+    rewrite_by_subst() %>%
+    rewrite_by_cond_subst()   # order of the function calls matters
 }
 
 
@@ -30,26 +31,17 @@ subst_rules <- function() {
   make_rule <- function(from, to) {
     function(x) subst(x, pattern = from, replacement = to)
   }
-
   list(
+    # Binary operators
     make_rule("<-", "="),
     make_rule("<<-", "="),
     make_rule("^", "**"),
-    make_rule("pi", "Math.pi"),
-    make_rule("self", "this"),
     make_rule("%%", "%"),
     make_rule("$", "."),
     make_rule(":", "R.seq_by"),
-    make_rule("seq", "R.seq_by"),
-    make_rule("c", "R.c"),
-    make_rule("matrix", "R.matrix"),
-    make_rule("print", "R.print"),
-    make_rule("length", "R.length"),
-    # make_rule("NULL", "null"),   # doesn't work since R doesn't distinguish input NULL and empty NULL.
-    make_rule("TRUE", "true"),
-    make_rule("FALSE", "false"),
-    make_rule("declare", "let"),
-    make_rule("%instanceof%", "instanceof")
+    make_rule("%instanceof%", "instanceof"),
+    make_rule("%=>%", "=>"),
+    make_rule("self", "this")
   )
 }
 
@@ -72,7 +64,68 @@ subst <- function(ast, pattern, replacement) {
 }
 
 
-# Rewriting AST (special cases)
+# Conditional substitute
+rewrite_by_cond_subst <- function(ast) {
+  magrittr::freduce(ast, cond_subst_rules())
+}
+
+cond_subst_rules <- function() {
+  make_rule <- function(from, to) {
+    function(x) cond_subst(x, pattern = from, replacement = to)
+  }
+  list(
+    # Base Javascript
+    make_rule("TRUE", "true"),
+    make_rule("FALSE", "false"),
+    make_rule("declare", "let"),
+    # R-like functions
+    make_rule(   "seq", "R.seq_by"),
+    make_rule(     "c", "R.c"),
+    make_rule("matrix", "R.matrix"),
+    make_rule( "print", "R.print"),
+    make_rule("length", "R.length"),
+    # Math.js
+    make_rule(  "pi", "Math.pi"),
+    make_rule( "sin", "Math.sin"),
+    make_rule( "cos", "Math.cos"),
+    make_rule( "tan", "Math.tan"),
+    make_rule( "sinh", "Math.sinh"),
+    make_rule( "cosh", "Math.cosh"),
+    make_rule( "tanh", "Math.tanh"),
+    make_rule("asin", "Math.asin"),
+    make_rule("acos", "Math.acos"),
+    make_rule("atan", "Math.atan"),
+    make_rule("asinh", "Math.asinh"),
+    make_rule("acosh", "Math.acosh"),
+    make_rule("atanh", "Math.atanh")
+    # make_rule("NULL", "null"),   # doesn't work since R doesn't distinguish input NULL and empty NULL.
+  )
+}
+
+cond_subst <- function(ast, pattern, replacement) {
+  if (rlang::is_call(ast)) {
+    if (rlang::is_call(ast, '.')) {
+      return(ast)
+    } else {
+      return(as.call(purrr::map(ast, ~cond_subst(.x, pattern, replacement))))
+    }
+  }
+
+  if (rlang::is_symbol(ast, pattern)) {
+    return(as.symbol(replacement))
+  }
+
+  if (rlang::is_syntactic_literal(ast)) {
+    if (is.null(ast))   return(ast)
+    if (is.na(ast))     return(ast)
+    if (ast == pattern) return(as.symbol(replacement))
+  }
+
+  ast
+}
+
+
+# Rewriting AST (special form)
 rewrite_new <- function(ast) {
   if (rlang::is_call(ast)) {
     if (rlang::is_call(ast, "$")) {

c460d7674ebd1fea4f5ade6538a93835c9eab9e1
kcf-jackson <kcf.jackson@gmail.com>
2019-10-28 04:08:34 +1100

    Add d3.js link

--- R/template.R
+++ R/template.R
@@ -28,6 +28,15 @@ src <- function(x) {
         "math" = "https://cdnjs.cloudflare.com/ajax/libs/mathjs/6.2.2/math.min.js",
         "p5" = "https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.9.0/p5.js",
         "plotly" = "https://cdn.plot.ly/plotly-latest.min.js",
-        "chart" = "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
+        "chart" = "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
+        "d3" = "https://d3js.org/d3.v5.min.js"
     )
 }
+
+
+#' Empty functions
+#' @description This function does nothing. It is created to ensure
+#' the keywords `let` and `declare` are defined.
+#' @param ... Any arguments
+#' @export
+let <- declare <- function(...) { invisible(NULL) }

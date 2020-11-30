39adc4a0be11ae034adb5669200737e10a30c78d
kcf-jackson <kcf.jackson@gmail.com>
2020-12-01 01:53:42 +1100

    Add web fonts and information about licenses

--- R/utils-helpers.R
+++ R/utils-helpers.R
@@ -10,21 +10,45 @@
 #' @export
 src <- function(x) {
     switch(x,
+           # Math
            "mathjs" = "https://cdnjs.cloudflare.com/ajax/libs/mathjs/7.0.2/math.min.js",
+           "tensorflow" = "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@2.0.0/dist/tf.min.js",
+           # Visualisation
            "p5" = "https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.0.0/p5.min.js",
            "chart" = "https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js",
            "plotly" = "https://cdn.plot.ly/plotly-latest.min.js",
            "d3" = "https://d3js.org/d3.v5.min.js",
            "vegalite" = "https://cdn.jsdelivr.net/npm/vega-lite@4.15.0/build/vega-lite.min.js",
-           "tensorflow" = "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@2.0.0/dist/tf.min.js",
+           # Style sheets and Icons
+           "fontawesome" = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css",
+           "ionicons" = "https://unpkg.com/ionicons@5.2.3/dist/ionicons.js",
+           # Modules
            "dom" = system.file("modules/dom.js", package = "sketch"),
+           "websocket" = system.file("modules/websocket.js", package = "sketch"),
+           "testthat" = system.file("modules/testthat.js", package = "sketch"),
            stop(glue::glue("Library '{x}' does not exist."))
+    )
+}
+
+
+license_info <- function(x) {
+    switch(x,
+           "mathjs" = list(license = "Apache-2.0", url = "https://github.com/josdejong/mathjs/blob/develop/LICENSE"),
+           "tensorflow" = list(license = "Apache-2.0", url = "https://github.com/tensorflow/tfjs/blob/master/LICENSE"),
+           "p5" = list(license = "LGPL-2.1-only", url = "https://p5js.org/copyright.html"),
+           "chart" = list(license = "MIT", url = "https://github.com/chartjs/Chart.js/blob/master/LICENSE.md"),
+           "plotly" = list(license = "MIT", url = "https://github.com/plotly/plotly.js/blob/master/LICENSE"),
+           "d3" = list(license = "BSD-3-Clause", url = "https://github.com/d3/d3/blob/master/LICENSE"),
+           "vegalite" = list(license = "BSD-3-Clause", url = "https://github.com/vega/vega-lite/blob/master/LICENSE"),
+           "fontawesome" = list(license = "MIT", url = "https://fontawesome.com/v4.7.0/license/"),
+           "ionicons" = list(license = "MIT", url = "https://unpkg.com/browse/ionicons@5.2.3/LICENSE")
     )
 }
 
 
 #' Empty functions
 #'
+#' @name let-declare-const
 #' @rdname empty-functions
 #' @description These functions do nothing. It is created to ensure
 #' the keywords `let` and `declare` are defined.
@@ -44,6 +68,12 @@ let <- function(...) { invisible(NULL) }
 #' @rdname empty-functions
 #' @export
 declare <- function(...) { invisible(NULL) }
+
+
+#' @rdname empty-functions
+#' @export
+const <- function(...) { invisible(NULL) }
+
 
 
 #' Parse R code

b3c946d28e30329e2d9eae0420cdd7138915b92b
kcf-jackson <kcf.jackson@gmail.com>
2020-12-06 17:51:26 +1100

    Add test for R6Class, knitr engine and safeguard

---
+++ tests/testthat/test_check.R
@@ -0,0 +1,20 @@
+testthat::context("Test pre-transpilation check")
+
+test_that("Test reserved words are not assigned any value and not
+          used as function arguments", {
+    # basic_rules
+    unit_test_basic <- function(expr) {
+        safeguard(parse_expr(expr), rules = basic_rules())
+    }
+    expect_silent(unit_test_basic("max <- 3"))
+    expect_warning(unit_test_basic("pipe <- 3"))
+
+    # default_rules
+    unit_test_default <- function(expr) {
+        safeguard(parse_expr(expr), rules = default_rules())
+    }
+    expect_silent(unit_test_default("list(max = 3)"))
+    expect_warning(unit_test_default("max <- 3"))
+    expect_warning(unit_test_default("max = 3"))
+    expect_warning(unit_test_default("function(min = 0, max = 1) {}"))
+})

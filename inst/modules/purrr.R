e828105aa0b0e2b1f08e6764aa9bd615ca7d0cef
kcf-jackson <kcf.jackson@gmail.com>
2021-02-08 06:37:38 +1100

    Add higher order functions for the "basic" case

---
+++ inst/modules/purrr.R
@@ -0,0 +1,27 @@
+map <- function(x, f) {
+    return(x$map(f))
+}
+
+reduce <- function(x, f) {
+    return(x$reduce(f))
+}
+
+reduce_right <- function(x, f) {
+    return(x$reduceRight(f))
+}
+
+filter <- function(x, f) {
+    return(x$filter(f))
+}
+
+walk <- function(x, f) {
+    return(x$forEach(walk))
+}
+
+all <- function(x, f) {
+    return(x$every(f))
+}
+
+any <- function(x, f) {
+    return(x$some(f))
+}
